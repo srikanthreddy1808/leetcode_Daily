1e4199271da17bc512d81863da06d8a53a5397d4
srikanthreddy <srikanthreddyn559@gmail.com>
2026-03-24 15:49:12 +0530

    Time: 11 ms (58.33%), Space: 103.4 MB (16.18%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] constructProductMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int mod = 12345;
+        int size = n * m;
+
+        int[] arr = new int[size];
+        int idx = 0;
+        for(int[] row : grid){
+            for(int val : row){
+                arr[idx++] = val % mod;
+            }
+        }
+
+        int[] prefix = new int[size];
+        prefix[0] = 1;
+        for(int i = 1; i < size; i++){
+            prefix[i] = (int)((long)prefix[i - 1] * arr[i - 1]) % mod;
+        }
+
+        int[] suffix = new int[size];
+        suffix[size - 1] = 1;
+        for(int i = size - 2; i >= 0; i--){
+            suffix[i] = (int)((long)suffix[i + 1] * arr[i + 1]) % mod;
+        }
+
+        int[][] result = new int[n][m];
+        idx = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                result[i][j] = (int)((long)prefix[idx] * suffix[idx]) % mod;
+                idx++;
+            }
+        }
+        return result;
+    }
+}
